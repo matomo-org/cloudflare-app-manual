cc8ee6e54e22214ef01fc5766fef1b4942f58e55
Lukas Winkler <git@lw1.at>
2021-04-27 11:55:52 +0200

    use var instead of let/const

--- source/app.js
+++ source/app.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict'
 
-  const options = INSTALL_OPTIONS
+  var options = INSTALL_OPTIONS
 
   window._paq = window._paq || []
 
@@ -20,7 +20,7 @@
     window._paq.push(['setDoNotTrack', true])
   }
 
-  let domain = String(options.piwikdomain)
+  var domain = String(options.piwikdomain)
 
   if (domain.indexOf('http') !== 0 && domain.indexOf('//') !== 0) {
     domain = 'https://' + domain
@@ -33,8 +33,8 @@
   window._paq.push(['setTrackerUrl', domain + 'matomo.php'])
   window._paq.push(['setSiteId', options.piwikidsite])
 
-  const piwikVendorScript = document.createElement('script')
-  const firstScriptOnPage = document.getElementsByTagName('script')[0]
+  var piwikVendorScript = document.createElement('script')
+  var firstScriptOnPage = document.getElementsByTagName('script')[0]
   piwikVendorScript.src = domain + 'matomo.js'
   piwikVendorScript.async = true
   firstScriptOnPage.parentNode.insertBefore(piwikVendorScript, firstScriptOnPage)
